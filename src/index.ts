df638e29b1b4b07aaaa3b8ff886616a5f469c107
ahazimeh01 <ali.h@echovalley.io>
2023-12-10 23:21:14 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -127,8 +127,8 @@ app.post("/register", async (req, res) => {
   }
 });
 app.post("/insertProductAlternative", async (req, res) => {
-  const allBrands = await Brand.find({
-    where: { brandSearch: { completed: false } },
+  const allBrands = await Brand.findBy({
+    brandSearch: { completed: false },
   });
   return res.send({ hi: "" });
   // return res.send({ allBrands });
